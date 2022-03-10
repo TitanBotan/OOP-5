92b7d1309c3ee699986912be8a5719b038638415
Максим Титов <Tit0vMaksim@yandex.ru>
2022-03-10 18:55:33 +0300

    Добавьте файлы проекта.

---
+++ ООП 5/Header.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+
+using std::cout;
+using std::endl;
+float N1(float a, float b);
+float N2(float a, float b);
+float findN(float a, float b);
+float M1(float a, float b, float n);
+float M2(float a, float b, float n);
+float M3(float a, float b, float n);
+float findM(float a, float b, float n);
---
+++ ООП 5/ООП 5.cpp
@@ -0,0 +1,18 @@
+﻿//Вариант 1 (По списку 20)
+#include <iostream>
+#include <cstdlib>
+#include <windows.h>
+#include <cmath>
+#include "Header.h"
+using namespace std;
+void main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	float a, b, m, n;
+	cout << "Введите числа a и b: \n";
+	cin >> a >> b;
+	n = findN(a, b);
+	m = findM(a, b, n);
+	cout << "a = " << a << "  b = " << b << "  n = " << n << "  m = " << m;
+}
